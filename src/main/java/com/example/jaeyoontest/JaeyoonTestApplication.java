fda237fd70e31d2293b1a238b6845a732da65ebb
siyoung <siyoung@woowahan.com>
2021-04-27 21:22:57 +0900

    테스트 리파지토리 생성 완료

---
+++ src/main/java/com/example/jaeyoontest/JaeyoonTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jaeyoontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaeyoonTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaeyoonTestApplication.class, args);
+    }
+
+}
